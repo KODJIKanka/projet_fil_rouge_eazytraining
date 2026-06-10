db3df99e8120b7432d43bb3fd945984d4780044c
KODJI KANKA Paul <kodjidjaba@gmail.com>
2026-06-10 22:20:04 +0100

    Module security_group

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -9,5 +9,13 @@ resource "aws_security_group" "sg_project"{
             cidr_blocks = var.cidr_blocks
         }
     }
-    egress
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+      Name = var.security_groups_name
+    }
 }
---
+++ terraform/modules/security_group/variables.tf
@@ -0,0 +1,16 @@
+variable "cidr_blocks" {
+    default = ["0.0.0.0/0"] 
+}
+
+variable "security_groups_name" {
+
+  
+}
+
+variable "security_groups_ports" {
+
+  
+}
+
+variable "protocol" {
+}
